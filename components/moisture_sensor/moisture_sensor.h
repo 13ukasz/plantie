f6997632ca3430c67fb4e27c083c3dd26991f1d5
13ukasz <lukas.bogacz12345@gmail.com>
2025-04-15 22:25:21 +0200

    Move ADC read to /components

---
+++ components/moisture_sensor/moisture_sensor.h
@@ -0,0 +1,6 @@
+#ifndef MOISTURE_SENSOR_H
+#define MOISTURE_SENSOR_H
+
+void moisture_sensor_fun(void *pvParameters);
+
+#endif
--- main/main.c
+++ main/main.c
@@ -4,21 +4,13 @@
 #include "driver/adc.h"
 #include "esp_log.h"
 
+#include "moisture_sensor.h"
+
 static const char *TAG = "ADC_EXAMPLE";
 
 void app_main(void)
 {
-    // Configure ADC1 channel 0 (GPIO0)
-    adc1_config_width(ADC_WIDTH_BIT_12);  // 12-bit resolution
-    adc1_config_channel_atten(ADC1_CHANNEL_0, ADC_ATTEN_DB_11); // wider voltage range ~0-3.3V
+    ESP_LOGI(TAG, "Starting ADC Task...");
 
-    while (1) {
-        // Read raw ADC value
-        int raw = adc1_get_raw(ADC1_CHANNEL_0);
-
-        // Print value
-        ESP_LOGI(TAG, "ADC Raw: %d", raw);
-
-        vTaskDelay(pdMS_TO_TICKS(1000)); // 1 second delay
-    }
+    xTaskCreate(moisture_sensor_fun, "moisture_sensor_fun", 2048, NULL, 1, NULL);
 }
