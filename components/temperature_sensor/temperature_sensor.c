fe839dc089012f42d8669051835f046d3e2ec086
13ukasz <lukas.bogacz12345@gmail.com>
2025-05-11 19:11:46 +0200

    Add temperature sensor dependencies

---
+++ components/temperature_sensor/temperature_sensor.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "ds18b20.h"
+#include "onewire_bus.h"
---
+++ components/temperature_sensor/temperature_sensor.h
@@ -0,0 +1,6 @@
+#ifndef TEMPERATURE_SENSOR_H
+#define TEMPERATURE_SENSOR_H
+
+#include "ds18b20_types.h"
+
+#endif TEMPERATURE_SENSOR_H
