74cceba1328874a50fc6d38363c602ba4342eaa1
13ukasz <lukas.bogacz12345@gmail.com>
2025-05-13 08:57:43 +0200

    Add zigbee task

--- main/main.c
+++ main/main.c
@@ -7,9 +7,11 @@
 
 #include "moisture_sensor.h"
 #include "temperature_sensor.h"
+#include "zigbee_relay.h"
 
 void app_main(void)
 {
     xTaskCreate(moisture_sensor_task, "moisture_sensor_fun", 2048, NULL, 1, NULL);
     xTaskCreate(sensor_read_task, "sensor_read_task", 4096, NULL, 5, NULL);
+    xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 5, NULL);
 }
