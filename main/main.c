417d808c9107e71ffda35806c131515e3f35f2e6
13ukasz <lukas.bogacz12345@gmail.com>
2025-06-16 12:38:27 +0200

    Add sample web-server

--- main/main.c
+++ main/main.c
@@ -7,11 +7,13 @@
 
 #include "moisture_sensor.h"
 #include "temperature_sensor.h"
+#include "web_server.h"
 #include "zigbee_relay.h"
 
 void app_main(void)
 {
-    xTaskCreate(moisture_sensor_task, "moisture_sensor_fun", 2048, NULL, 1, NULL);
-    xTaskCreate(sensor_read_task, "sensor_read_task", 4096, NULL, 5, NULL);
+    xTaskCreate(moisture_sensor_task, "moisture_sensor_fun", 2048, NULL, 2, NULL);
+    // xTaskCreate(sensor_read_task, "sensor_read_task", 4096, NULL, 5, NULL);
     xTaskCreate(esp_zb_task, "Zigbee_main", 4096, NULL, 5, NULL);
+    xTaskCreate(web_server_task, "web_server_task", 4096, NULL, 1, NULL);
 }
